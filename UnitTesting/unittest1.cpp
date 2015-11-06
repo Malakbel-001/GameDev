c7cbc3bb0657511d7bc73073670d2d10fc4ecc97
D-Eijkelenboom <eijkelenboomdaniel@hotmail.com>
2015-11-06 22:02:10 +0100

    setup for new tes

--- UnitTesting/unittest1.cpp
+++ UnitTesting/unittest1.cpp
@@ -1,6 +1,8 @@
 #include "stdafx.h"
 #include "CppUnitTest.h"
 #include "../GameDev/InputManager.h"
+#include "../GameDev/BehaviourFactory.h"
+#include "../GameDev/EntityType.h"
 
 using namespace Microsoft::VisualStudio::CppUnitTestFramework;
 
@@ -12,9 +14,14 @@ namespace UnitTesting
 		
 		TEST_METHOD(TestMethod1)
 		{
-			InputManager* manager = new InputManager();
-			//Assert::AreEqual(expecte)
-		}
+			int screenheight = 600;
+			int screenwidth = 800;
 
+			SDL_Window* window = SDL_CreateWindow("titel", SDL_WINDOWPOS_CENTERED, SDL_WINDOWPOS_CENTERED, screenheight, screenwidth, false);
+			SDL_Renderer* renderer = SDL_CreateRenderer(window, -1, 0);
+
+			BehaviourFactory* factory = new BehaviourFactory(renderer, screenwidth, screenheight);
+			DrawableBehaviour* behaviour = factory->CreateDrawableBehaviour(EntityType::PLAYER);
+		}
 	};
 }
