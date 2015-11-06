97732f209115c16e54cdb778d1caeccc09848868
mjowned <mj65@live.nl>
2015-11-06 17:41:31 +0100

    wasd + escape

--- UnitTesting/inputTest.cpp
+++ UnitTesting/inputTest.cpp
@@ -15,8 +15,14 @@ namespace UnitTesting
 			InputManager* manager = new InputManager();
 			manager->SetKeyInput(SDLK_w);
 			manager->SetKeyInput(SDLK_d);
+			manager->SetKeyInput(SDLK_a);
+			manager->SetKeyInput(SDLK_s);
+			manager->SetKeyInput(SDLK_ESCAPE);
 			//Expected Key_Input SDLK_w
 			Assert::AreEqual(true, manager->GetKeyInput()->at(SDLK_w));
+			Assert::AreEqual(true, manager->GetKeyInput()->at(SDLK_a));
+			Assert::AreEqual(true, manager->GetKeyInput()->at(SDLK_ESCAPE));
+			Assert::AreEqual(true, manager->GetKeyInput()->at(SDLK_s));
 			Assert::AreEqual(true, manager->GetKeyInput()->at(SDLK_d));
 		}
 
