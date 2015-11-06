c34a4da3e87e1b96d234903cc78dec3d7a8e4376
rhsattev <rhs.vanatteveld@student.avans.nl>
2015-11-06 21:28:38 +0100

    lower enemies placed

--- GameDev/testLevel.cpp
+++ GameDev/testLevel.cpp
@@ -10,6 +10,7 @@ void TestLevel::Init(BehaviourFactory* bf)
 {
 
 	entityFactory = new EntityFactory(*world, bf, drawableContainer);
+
 	//obstacles--------------
 	entityFactory->CreateEntity(363, 450, 137, 120, EntityType::GROUND2);
 	entityFactory->CreateEntity(750, 450, 250, 120, EntityType::GROUND);
@@ -22,11 +23,13 @@ void TestLevel::Init(BehaviourFactory* bf)
 	entityFactory->CreateEntity(4000, 450, 250, 10, EntityType::BAR);
 	entityFactory->CreateEntity(4363, 450, 137, 120, EntityType::GROUND2);
 	entityFactory->CreateEntity(4495, 450, 250, 120, EntityType::GROUND);
+
 	//ground------------------
 	entityFactory->CreateEntity(0, 570, 250, 140, EntityType::GROUND);
 	entityFactory->CreateEntity(250, 570, 250, 140, EntityType::GROUND);
 	entityFactory->CreateEntity(500, 570, 250, 140, EntityType::GROUND);
 	entityFactory->CreateEntity(750, 570, 250, 140, EntityType::GROUND);
+
 	entityFactory->CreateEntity(1000, 570, 250, 140, EntityType::GROUND);
 	entityFactory->CreateEntity(1250, 570, 250, 140, EntityType::GROUND);
 	entityFactory->CreateEntity(1500, 570, 250, 140, EntityType::GROUND);
@@ -44,14 +47,22 @@ void TestLevel::Init(BehaviourFactory* bf)
 	entityFactory->CreateEntity(4500, 570, 250, 140, EntityType::GROUND);
 	entityFactory->CreateEntity(4750, 570, 250, 140, EntityType::GROUND);
 	//enemies-----------------
-	entityFactory->CreateEntity(150, 600, 40, 45, EntityType::PLANT);
+	
 	entityFactory->CreateEntity(800, 450, 40, 45, EntityType::PLANT);
 	entityFactory->CreateEntity(900, 450, 40, 45, EntityType::PLANT);
 	entityFactory->CreateEntity(1200, 300, 40, 45, EntityType::PLANT);
 	entityFactory->CreateEntity(1300, 300, 40, 45, EntityType::PLANT);
+
+	// enemies on top------------------------------------------------
 	entityFactory->CreateEntity(2050, 300, 40, 45, EntityType::PLANT);
 	entityFactory->CreateEntity(2150, 300, 40, 45, EntityType::PLANT);
 	entityFactory->CreateEntity(2250, 300, 40, 45, EntityType::PLANT);
+
+	// enemies on below------------------------------------------------
+	entityFactory->CreateEntity(2050, 600, 40, 45, EntityType::PLANT);
+	entityFactory->CreateEntity(2150, 600, 40, 45, EntityType::PLANT);
+	entityFactory->CreateEntity(2250, 400, 40, 45, EntityType::PLANT);
+
 	entityFactory->CreateEntity(2900, 300, 40, 45, EntityType::PLANT);
 	entityFactory->CreateEntity(3000, 300, 40, 45, EntityType::PLANT);
 
@@ -67,7 +78,7 @@ void TestLevel::Init(BehaviourFactory* bf)
 
 TestLevel::~TestLevel()
 {
-	//	delete tileLoader;
+	delete tileLoader;
 }
 
 Level* TestLevel::CreateLevel()
@@ -79,4 +90,3 @@ void TestLevel::Cleanup() {}
 
 
 void TestLevel::HandleEvents(SDL_Event mainEvent) { }
-
