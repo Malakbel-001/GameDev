e8287318edcde9b6183f182883ed22b579de3ff4
mjowned <mj65@live.nl>
2015-12-03 15:25:07 +0100

    small commit

--- GameDev/EntityFactory.h
+++ GameDev/EntityFactory.h
@@ -23,7 +23,7 @@ public:
 	Player* CreatePlayer(int _hitdmg, int _healt, float x, float y, float height, float width, Player* _player);
 	Bullet* CreateBullet(float x, float y, int width, int height, int dmg, b2Vec2 direction, EntityType type);
 	b2Body* CreateActorBody(float x, float y, float height, float width, float den, EntityType type);
-	b2Body* CreateBody(float x, float y, float height, float width, EntityType type);	
+	b2Body* CreateBody(float x, float y, float height, float width, EntityType type);
 	Weapon* CreateWeapon(float x, float y, EntityType type);
 	b2Body* CreateBody(float x, float y, float height, float width, float den, EntityType type);
 
